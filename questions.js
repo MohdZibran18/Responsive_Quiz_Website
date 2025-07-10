9d30c231f468c2aaa39dbd2e96ec6e966da2c0eb
MohdZibran18 <2k22.it.32328@gmail.com>
2025-07-11 01:56:28 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,333 @@
+//questions.js
+let questions = [
+    {
+        numb: 1,
+        question: " Is Python code compiled or interpreted?",
+        answer: "a) Python code is both compiled and interpreted",
+        options: [
+            "a) Python code is both compiled and interpreted",
+            "b) Python code is neither compiled nor interpreted",
+            "c) Python code is only compiled",
+            "d) Python code is only interpreted"
+        ]
+    },
+    {
+        numb: 2,
+        question: "All keywords in Python are in _________",
+        answer: "d) None of the mentioned" ,
+        options: [
+          "a) Capitalized",
+          "b) lower case",
+          "c) UPPER CASE",
+          "d) None of the mentioned"  
+        ]
+    },
+    {
+        numb: 3,
+        question: " Which of the following is used to define a block of code in Python language?",
+        answer: "a) Indentation",
+        options: [
+            "a) Indentation",
+            "b) Key",
+            "c) Brackets",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which keyword is used for function in Python language?",
+        answer: "b) def",
+        options: [
+            "a) Function",
+            "b) def",
+            "c) Fun",
+            "d) Define"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following character is used to give single-line comments in Python?",
+        answer: "b) #",
+        options: [
+            "a) //",
+            "b) #",
+            "c) !",
+            "d) /*"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Python supports the creation of anonymous functions at runtime, using a construct called __________",
+        answer: "c) lambda",
+        options: [
+          "a) pi",
+          "b) anonymous",
+          "c) lambda",
+          "d) none of the mentioned"  
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the order of precedence in python?",
+        answer: "d) Parentheses, Exponential, Multiplication, Division, Addition, Subtraction",
+        options: [
+            "a) Exponential, Parentheses, Multiplication, Division, Addition, Subtraction",
+            "b) Exponential, Parentheses, Division, Multiplication, Addition, Subtraction",
+            "c) Parentheses, Exponential, Multiplication, Division, Subtraction, Addition",
+            "d) Parentheses, Exponential, Multiplication, Division, Addition, Subtraction"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which of the following is true for variable names in Python?",
+        answer: "b) unlimited length",
+        options: [
+            "a) underscore and ampersand are the only two special characters allowed",
+            "b) unlimited length",
+            "c) all private members must have leading and trailing underscores",
+            "d) none of the mentioned"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of the following is the truncation division operator in Python?",
+        answer: "b) //",
+        options: [
+            "a) |",
+            "b) //",
+            "c) /",
+            "d) %"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which of the following functions is a built-in function in python?",
+        answer:  "b) print()",
+        options: [
+            "a) factorial()",
+            "b) print()",
+            "c) seed()",
+            "d) sqrt()"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which of the following is the use of id() function in python?",
+        answer: "b) Id returns the identity of the object",
+        options: [
+            "a) Every object doesn’t have a unique id",
+            "b) Id returns the identity of the object",
+            "c) All of the mentioned",
+            "d) None of the mentioned"
+        ]
+    },
+    {
+        numb: 12,
+        question: " Which of the following is not a core data type in Python programming?",
+        answer: "c) Class",
+        options: [
+            "a) Tuples",
+            "b) Lists",
+            "c) Class",
+            "d) Dictionary"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Which of these is the definition for packages in Python?",
+        answer:  "b) A folder of python modules",
+        options: [
+            "a) A set of main modules",
+            "b) A folder of python modules",
+            "c) A number of files containing Python definitions and statements",
+            "d) A set of programs making use of Python modules"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which one of the following is not a keyword in Python language?",
+        answer: "b) eval",
+        options: [
+            "a) pass",
+            "b) eval",
+            "c) assert",
+            "d) nonlocal"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What arithmetic operators cannot be used with strings in Python?",
+        answer: "a) -",
+        options: [
+            "a) -",
+            "b) *",
+            "c) +",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb: 16,
+        question: " Which one of the following is not a keyword in Python language?",
+        answer: "b) eval",
+        options: [
+            "a) pass",
+            "b) eval",
+            "c) assert",
+            "d) nonlocal"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Which of the following statements is used to create an empty set in Python?",
+        answer: "d) set()",
+        options: [
+            "a) ( )",
+            "b) [ ]",
+            "c) { }",
+            "d) set()"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What is the maximum possible length of an identifier in Python?",
+        answer:  "d) none of the mentioned",
+        options: [
+            "a) 79 characters",
+            "b) 31 characters",
+            "c) 63 characters",
+            "d) none of the mentioned"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What are the two main types of functions in Python?",
+        answer: "c) Built-in function & User defined function",
+        options: [
+            "a) System function",
+            "b) Custom function",
+            "c) Built-in function & User defined function",
+            "d) User function"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Which of the following is a feature of Python DocString?",
+        answer: "d) All of the mentioned",
+        options: [
+            "a) In Python all functions should have a docstring",
+            "b) Docstrings can be accessed by the __doc__ attribute on objects",
+            "c) It provides a convenient way of associating documentation with Python modules, functions, classes, and methods",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What is output of print(math.pow(3, 2))",
+        answer: "a) 9.0",
+        options: [
+            "a) 9.0",
+            "b) None",
+            "c) 9",
+            "d) None of the mentioned"
+        ]
+    },
+    {
+        numb: 22,
+        question:"Which of the following is the use of id() function in python?",
+        answer: "b) In Python Id function returns the identity of the object",
+        options: [
+            "a) Every object in Python doesn’t have a unique id",
+            "b) In Python Id function returns the identity of the object",
+            "c) None of the mentioned",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb: 23,
+        question: "The process of pickling in Python includes ____________",
+        answer: "a) conversion of a Python object hierarchy into byte stream",
+        options: [
+            "a) conversion of a Python object hierarchy into byte stream",
+            "b) conversion of a datatable into a list",
+            "c) conversion of a byte stream into Python object hierarchy",
+            "d) conversion of a list into a datatable"
+        ]
+    },
+    {
+        numb: 24,
+        question: "What will be the output of the following Python expression? round(4.576)",
+        answer: "c) 5",
+        options: [
+            "a) 4",
+            "b) 4.6",
+            "c) 5",
+            "d) 4.5"
+        ]
+    },
+    {
+        numb: 25,
+        question: "Which of the following Python statements will result in the output: 6? A = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]",
+        answer: "b) A[1][2]",
+        options: [
+            "a) A[2][1]",
+            "b) A[1][2]",
+            "c) A[3][2]",
+            "d) A[2][3]"
+        ]
+    },
+    {
+        numb: 26,
+        question: "Which are the advantages of functions in python?",
+        answer: "d) All of the mentioned",
+        options: [
+            "a) Reducing duplication of code",
+            "b) Decomposing complex problems into simpler pieces",
+            "c) Improving clarity of the code",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb: 27,
+        question: "What are the two main types of functions?",
+        answer: "b) Built-in function & User defined function",
+        options: [
+            "a) Custom function",
+            "b) Built-in function & User defined function",
+            "c) User function",
+            "d) System function"
+        ]
+    },
+    {
+        numb: 28,
+        question: " Suppose list1 is [1, 3, 2], What is list1 * 2?",
+        answer: "c) [1, 3, 2, 1, 3, 2]",
+        options: [
+            "a) [2, 6, 4]",
+            "b) [1, 3, 2, 1, 3]",
+            "c) [1, 3, 2, 1, 3, 2]",
+            "d) [1, 3, 2, 3, 2, 1]"
+        ]
+    },
+    {
+        numb: 29,
+        question: "Suppose list1 = [0.5 * x for x in range(0, 4)], list1 is:",
+        answer: "c) [0.0, 0.5, 1.0, 1.5]",
+        options: [
+            "a) [0, 1, 2, 3]",
+            "b) [0, 1, 2, 3, 4]",
+            "c) [0.0, 0.5, 1.0, 1.5]",
+            "d) [0.0, 0.5, 1.0, 1.5, 2.0]"
+        ]
+    },
+    {
+        numb: 30,
+        question: "To insert 5 to the third position in list1, we use which command?",
+        answer: "b) list1.insert(2, 5)",
+        options: [
+            "a) list1.insert(3, 5)",
+            "b) list1.insert(2, 5)",
+            "c) list1.add(3, 5)",
+            "d) list1.append(3, 5)"
+        ]
+    },
+];
